229e8164c78ce68606569497e84915381e24ba9c
rashmi11mokashi <rashmimokashi2099@gmail.com>
2024-11-02 09:09:10 +0900

    Added weather API integration for current weather

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const axios = require('axios')
-require('dotenv').config();
+
+const dotenv = require("dotenv")
+dotenv.config();
+//require('dotenv').config();
 //dotenv is module to load env variables from .env file into process.env 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.get('/', (req, res) => {
     res.send('Weather API Wrapper Service is running');
@@ -13,3 +17,21 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`); // these `` make a difference
 })
+
+app.get('/weather/current', async(req, res) => {
+    const city = req.query.city;
+    if (!city) {
+        return
+        res.status(400).json({ error: 'City is required' });
+    }
+
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+            params: { q: city, appid: process.env.WEATHER_API_KEY, units: 'metric' }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve data' });
+    }
+    res.send('Fetching weather data...');
+});
